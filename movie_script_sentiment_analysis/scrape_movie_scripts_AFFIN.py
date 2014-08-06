865b3c07ce29cbe1c294fffef30199e7938fc0fa
tlfvincent <tlfvincent@gmail.com>
2014-08-06 09:36:40 -0400

    Fix bugs in script

--- movie_script_sentiment_analysis/scrape_movie_scripts_AFFIN.py
+++ movie_script_sentiment_analysis/scrape_movie_scripts_AFFIN.py
@@ -42,6 +42,15 @@ def ExtractMovieTitle():
 	results.append(query_year)
 	return(results)
 
+def FindMoviesWithMetadata():
+	# open new database to store IMDB and RT ratings data
+	temp_df = pd.DataFrame.from_csv('tmdb_metadata.tab', sep='\t', header=0, index_col=False)
+	# drop NAs in dataframe
+	tmdb_metadata = temp_df.dropna(subset=['budget', 'revenue', 'year', 'runtime', 'genre'])
+	# find remaining indices
+	valid_index = list(tmdb_metadata.index.values)
+	return(valid_index)
+
 def MovieSentimentCrawler():
 	'''
 	this functions parser through each movie script in the springfieldspringfield.co.uk
@@ -68,11 +77,15 @@ def MovieSentimentCrawler():
 		feeling_var FLOAT);''')
 	print "Table created successfully";
 
+	# find movies with available metadata in TMDB
+	valid_index = FindMoviesWithMetadata()
+
 	# define alphabet letters
 	alphabet = string.ascii_uppercase
 	alphabet = list(alphabet)
 
 	# parse through each letter on springfield website
+	index = 0
 	for letter in alphabet:
 		print 'Processing letter %s' % (str(letter))
 		page_init = 0
@@ -100,87 +113,90 @@ def MovieSentimentCrawler():
 			# grab all movie links on page
 			movie_links = soup.findAll('a', attrs={'class':'script-list-item'})
 			for link in movie_links:
+				# increment movie index
+				index += 1
+				if index in valid_index:
 
-				href_movie = link.get('href')
-				url_movie = 'http://www.springfieldspringfield.co.uk' + str(href_movie)
-				# extract mvie url title
-				url_title = href_movie.split('=')[1]
+					href_movie = link.get('href')
+					url_movie = 'http://www.springfieldspringfield.co.uk' + str(href_movie)
+					# extract mvie url title
+					url_title = href_movie.split('=')[1]
 
-				# get movie title and year of release
-				movie_info = link.contents
-				movie_info_content = re.split('[()]', movie_info[0]) 
-				title = movie_info_content[0].strip()
-				title = multipleReplace(title, wordDict)
-				
-				# convert title to ASCII format
-				title = filter(lambda x: x in string.printable, title)
-				year = re.search('(\d{4})', movie_info[0]).group()
-				print title
-
-				html_test = requests.get(url_movie)
-				if html_test != 200:
-					corpus_sentiment = []
-					corpus_sentiment.append(title)
-					corpus_sentiment.append(url_title)
-					corpus_sentiment.append(year)
-					corpus_sentiment.append('NULL')
-					corpus_sentiment.append('NULL')
-					corpus_sentiment.append('NULL')
-					corpus_sentiment.append('NULL')
-					corpus_sentiment.append('NULL')
-					# insert into SQLITE database
-					db.execute('INSERT INTO MOVIE_SENTIMENT_AFFIN VALUES (?, ?, ?, ?, ?, ?, ?, ?)', corpus_sentiment)
-				else:
-					response = urllib2.urlopen(url_movie)
-					page_source = response.read()
-					page_source = re.sub('<br>',' ',page_source)
-					soup = BeautifulSoup(page_source)
-
-					# extract text lines from html
-					movie_script = soup.findAll('div', attrs={'class': 'scrolling-script-container'})
-					lines = movie_script[0].contents
-
-					# splits all script into individual sentences
-					sentences = tokenizer.tokenize(str(lines))
-
-					# delete last element of javascript content
-					del sentences[-1]
-					sentences = [w.lower() for w in sentences]
-					sentences = [w.replace('\\', '') for w in sentences]
-					new_sentence = []
+					# get movie title and year of release
+					movie_info = link.contents
+					movie_info_content = re.split('[()]', movie_info[0]) 
+					title = movie_info_content[0].strip()
+					title = multipleReplace(title, wordDict)
 					
-					# remove punctuation
-					for sentence in sentences:
-						new_sentence.append(multipleReplace(sentence.strip(), wordDict))
+					# convert title to ASCII format
+					title = filter(lambda x: x in string.printable, title)
+					year = re.search('(\d{4})', movie_info[0]).group()
+					print title
 
-					# find frequency of words in episode and print to file
-					word_distribution = FreqDist(new_sentence)
-					positive_sentiment = []
-					negative_sentiment = []
-					all_sentiment = []
-					for word in word_distribution.keys():
-						if word in word_score.keys():
-							#print '%s, %s, %s' % (str(word), str(word_distribution[word]), str(sentiment_sum))
-							sentiment_sum = int(word_distribution[word]) * int(word_score[word])
-							all_sentiment.append(sentiment_sum)
-							if sentiment_sum > 0:
-								positive_sentiment.append(sentiment_sum)
-							elif sentiment_sum <0 :
-								negative_sentiment.append(sentiment_sum)
+					html_test = requests.get(url_movie)
+					if html_test.status_code != 200:
+						corpus_sentiment = []
+						corpus_sentiment.append(title)
+						corpus_sentiment.append(url_title)
+						corpus_sentiment.append(year)
+						corpus_sentiment.append('NULL')
+						corpus_sentiment.append('NULL')
+						corpus_sentiment.append('NULL')
+						corpus_sentiment.append('NULL')
+						corpus_sentiment.append('NULL')
+						# insert into SQLITE database
+						db.execute('INSERT INTO MOVIE_SENTIMENT_AFFIN VALUES (?, ?, ?, ?, ?, ?, ?, ?)', corpus_sentiment)
+					else:
+						response = urllib2.urlopen(url_movie)
+						page_source = response.read()
+						page_source = re.sub('<br>',' ',page_source)
+						soup = BeautifulSoup(page_source)
 
-					# compuet sentiment data for movie script
-					corpus_sentiment = []
-					corpus_sentiment.append(title)
-					corpus_sentiment.append(url_title)
-					corpus_sentiment.append(year)
-					corpus_sentiment.append(np.sum(positive_sentiment))
-					corpus_sentiment.append(np.sum(negative_sentiment))
-					corpus_sentiment.append(np.sum(all_sentiment))
-					corpus_sentiment.append(np.mean(all_sentiment))
-					corpus_sentiment.append(np.var(all_sentiment))
+						# extract text lines from html
+						movie_script = soup.findAll('div', attrs={'class': 'scrolling-script-container'})
+						lines = movie_script[0].contents
 
-					# insert into SQLITE database
-					db.execute('INSERT INTO MOVIE_SENTIMENT_AFFIN VALUES (?, ?, ?, ?, ?, ?, ?, ?)', corpus_sentiment)
+						# splits all script into individual sentences
+						sentences = tokenizer.tokenize(str(lines))
+
+						# delete last element of javascript content
+						del sentences[-1]
+						sentences = [w.lower() for w in sentences]
+						sentences = [w.replace('\\', '') for w in sentences]
+						new_sentence = []
+						
+						# remove punctuation
+						for sentence in sentences:
+							new_sentence.append(multipleReplace(sentence.strip(), wordDict))
+
+						# find frequency of words in episode and print to file
+						word_distribution = FreqDist(new_sentence)
+						positive_sentiment = []
+						negative_sentiment = []
+						all_sentiment = []
+						for word in word_distribution.keys():
+							if word in word_score.keys():
+								#print '%s, %s, %s' % (str(word), str(word_distribution[word]), str(sentiment_sum))
+								sentiment_sum = int(word_distribution[word]) * int(word_score[word])
+								all_sentiment.append(sentiment_sum)
+								if sentiment_sum > 0:
+									positive_sentiment.append(sentiment_sum)
+								elif sentiment_sum <0 :
+									negative_sentiment.append(sentiment_sum)
+
+						# compuet sentiment data for movie script
+						corpus_sentiment = []
+						corpus_sentiment.append(title)
+						corpus_sentiment.append(url_title)
+						corpus_sentiment.append(year)
+						corpus_sentiment.append(np.sum(positive_sentiment))
+						corpus_sentiment.append(np.sum(negative_sentiment))
+						corpus_sentiment.append(np.sum(all_sentiment))
+						corpus_sentiment.append(np.mean(all_sentiment))
+						corpus_sentiment.append(np.var(all_sentiment))
+
+						# insert into SQLITE database
+						db.execute('INSERT INTO MOVIE_SENTIMENT_AFFIN VALUES (?, ?, ?, ?, ?, ?, ?, ?)', corpus_sentiment)
 
 	db.commit()
 	db.close()
